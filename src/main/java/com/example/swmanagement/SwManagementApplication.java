7013605e2cabcb597ebb94a88e0c94d5156f6f6c
Bangseungjae <tmdwo5825@naver.com>
2022-11-13 13:20:37 +0900

    Initial commit

---
+++ src/main/java/com/example/swmanagement/SwManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.swmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swmanagement/SwManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
