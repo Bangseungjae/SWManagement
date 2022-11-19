0defb71c68c55c8761b6a1235ea3b65a04982508
Bangseungjae <tmdwo5825@naver.com>
2022-11-19 15:44:57 +0900

    DB 정보 암호화

---
+++ src/main/java/com/example/swmanagement/config/JasyptConfig.java
@@ -0,0 +1,35 @@
+package com.example.swmanagement.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@EnableEncryptableProperties
+@Configuration
+public class JasyptConfig {
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
+
+    static final String JASYPT_STRING_ENCRYPTOR = "jasyptStringEncryptor";
+
+    @Primary
+    @Bean(JasyptConfig.JASYPT_STRING_ENCRYPTOR)
+    public StringEncryptor stringEncryptor() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize(1);
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        return encryptor;
+    }
+}
