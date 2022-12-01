503e5aeff709885e02fe72e3ff4ddc5ffd858286
Bangseungjae <tmdwo5825@naver.com>
2022-12-01 20:56:16 +0900

    api 문서 수정

--- src/main/java/com/example/swmanagement/controller/UserController.java
+++ src/main/java/com/example/swmanagement/controller/UserController.java
@@ -44,7 +44,7 @@ public class UserController {
 
     @GetMapping("/test")
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok().body("healthy5");
+        return ResponseEntity.ok().body("healthy6");
     }
 
 
