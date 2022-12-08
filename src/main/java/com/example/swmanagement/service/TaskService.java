c5ec029ed2825c2845a5040cbea61e73506c7fa6
Bangseungjae <tmdwo5825@naver.com>
2022-12-09 00:37:42 +0900

    task api수정

--- src/main/java/com/example/swmanagement/service/TaskService.java
+++ src/main/java/com/example/swmanagement/service/TaskService.java
@@ -13,11 +13,13 @@ import com.example.swmanagement.dto.task.TaskResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class TaskService {
 
     private final TaskRepository taskRepository;
